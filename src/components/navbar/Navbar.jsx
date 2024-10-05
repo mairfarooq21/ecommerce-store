3f5acb60f6e8b6387b3b0b8929ede559b3a245f7
Mair Farooq <159879384+mairfarooq21@users.noreply.github.com>
2024-10-05 00:28:41 -0700

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,8 +15,6 @@ function Navbar() {
 
   const user = JSON.parse(localStorage.getItem('user'));
 
-  // console.log(user.user.email)
-
   const logout = () => {
     localStorage.clear('user');
     window.location.href = '/Login'
@@ -102,10 +100,6 @@ function Navbar() {
       </Transition.Root>
 
       <header className="relative bg-white">
-        {/* <p className="flex h-10 items-center justify-center bg-pink-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8" 
-        style={{ backgroundColor: mode === 'dark' ? 'rgb(62 64 66)' : '', color: mode === 'dark' ? 'white' : '', }}>
-          Get free delivery on orders over $300
-        </p> */}
 
         <nav aria-label="Top" className="bg-gray-100 px-4 sm:px-6 lg:px-8 shadow-xl " style={{ backgroundColor: mode === 'dark' ? '#282c34' : '', color: mode === 'dark' ? 'white' : '', }}>
           <div className="">
@@ -155,35 +149,6 @@ function Navbar() {
                     Logout
                   </a> : ""}
                 </div>
-
-                {/* <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="flex items-center text-gray-700 ">
-                    <img
-                      src="https://ecommerce-sk.vercel.app/img/indiaflag.png"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium" style={{ color: mode === 'dark' ? 'white' : '', }}>INDIA</span>
-                  </a>
-                </div> */}
-                {/* <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="flex items-center text-gray-700 ">
-                    <img
-                      className="inline-block w-10 h-10 rounded-full"
-                      src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
-                      alt="Dan_Abromov" />
-                  </a>
-                </div> */}
-
-                {/* <div className="flex lg:ml-6">
-                  <button className='' onClick={toggleMode}>
-                    {mode === 'light' ?
-                      (<FiSun className='' size={30} />
-                      ) : 'dark' ?
-                        (<BsFillCloudSunFill size={30} />
-                        ) : ''}
-                  </button>
-                </div> */}
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
